fa2809e596fd2880716b20b8752beb9ec8d31e08
Glen <155974295+Glen-Ochieng@users.noreply.github.com>
2024-01-08 16:09:09 +0300

    Add files via upload

---
+++ Generation of event times histogram.R
@@ -0,0 +1,6 @@
+##The obvious initial step is to load in the dataset which I named 
+## the svl_dataset. It was a in Stata format thus for importation,
+## the package haven was instrumental.
+
+library(ggplot2)
+ggplot(data=svl_dataset,aes(event_duration))+ geom_histogram(fill="cyan4")+  labs(x ="Duration of contraceptive use (months)", y="Frequency")+ theme_bw()
