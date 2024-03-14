ea02b3d3ee81a228433139122850faf0016775f8
Glen <155974295+Glen-Ochieng@users.noreply.github.com>
2024-03-14 18:50:49 +0300

    Create Coding exercise 1.py

---
+++ Coding exercise 1.py
@@ -0,0 +1,7 @@
+## Q1: In the cell below variable "name" generates a random name. The variable "score" generates a random score.
+### • If the score is greater than or equal to 70 I want you to print:
+#### f" {name} perfomed really well in the test and got a score of {score}"
+### • If the score is equal to or above 50 but less than 70 print:
+#### f" {name} passed the test and got a score of {score}"
+### • If the score is less than 50 print:
+#### f" {name} failed the test with a score of {score}"
